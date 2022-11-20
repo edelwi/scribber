6068f3c5f36d3645038e441889711c4338b3bebf
Evgeniy Semenov <edelwi@yandex.ru>
2022-11-20 16:20:36 +0300

    fixed setup requirements missmatch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scribber",
-    version="v0.0.2",
+    version="v0.0.3",
     author="Evgeniy Semenov",
     author_email="edelwi@yandex.ru",
     description="A simple document generator with not very rich functionality that can export a document to "
@@ -16,11 +16,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     # entry_points={},
     install_requires=[
-        'lxml>=4.9.1,<5.0',
-        'pydantic>=1.10.2,<2.0',
-        'python-docx>=0.8.11,<1.0',
-        'typing_extensions>=4.4.0,<5.0'
-        'XlsxWriter>=3.0.3,<4.0'
+        'lxml>=4.8.0,<5.0.0',
+        'pydantic>=1.8.0,<2.0.0',
+        'python-docx>=0.8.0,<1.0.0',
+        'typing_extensions>=4.0.0,<5.0.0'
+        'XlsxWriter>=3.0.0,<4.0.0'
         ],
     classifiers=[
         "Development Status :: 1 - Planning",
