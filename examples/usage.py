8a331003b05c939127c65c54fa3d3d4a568597a3
Evgeniy Semenov <edelwi@yandex.ru>
2022-11-20 13:55:47 +0300

    created a fully functional package from my old script

---
+++ examples/usage.py
@@ -0,0 +1,56 @@
+from core.document import SimpleDocument, Title, EmptyLine, Paragraph, Table, Director
+from formats.excel.excel_document import ExcelDocumentBuilder
+from formats.markdown.markdown_document import MarkdownDocumentBuilder
+from formats.text.text_document import TextDocumentBuilder
+from formats.word.word_document import WordDocumentBuilder
+
+if __name__ == "__main__":
+    # Create abstract document
+    doc = SimpleDocument()
+    doc.add(Title(title="Funny report"))
+    doc.add(EmptyLine())
+    doc.add(Paragraph(text="Let me show you report"))
+    doc.add(
+        Table(
+            headers=['one', 'two', 'three', 'four'],
+            content=[
+                (1, 2, 3, 4),
+                ('234', 345, 986, ''),
+                (89, 35, 587643, 8675),
+            ]
+        )
+    )
+    doc.add(Title(title="Total", level=2))
+    doc.add(Paragraph(text="It's Ok!"))
+
+    director = Director()
+    text_report_builder = TextDocumentBuilder()
+    word_report_builder = WordDocumentBuilder()
+    excel_report_builder = ExcelDocumentBuilder()
+    marckdown_report_builder = MarkdownDocumentBuilder()
+
+    print("Make a Text Document")
+    director.builder = text_report_builder
+    director.build_report_from_doc(doc)
+    text_report_builder.parts.save('test.txt')
+
+    print("Make a Word Document")
+    director.builder = word_report_builder
+    director.build_report_from_doc(doc)
+    word_report_builder.parts.save('test.docx')
+
+    print("Make a Excel Document")
+    director.builder = excel_report_builder
+    director.build_report_from_doc(doc)
+    excel_report_builder.parts.save('test.xlsx')
+
+    print("Make a Marckdown Document")
+    director.builder = marckdown_report_builder
+    director.build_report_from_doc(doc)
+    marckdown_report_builder.parts.save('test.md')
+
+    print()
+    print('Build without Director')
+    text_report_builder.add_title(Title(title="Next report"))
+    text_report_builder.add_paragraph(Paragraph(text="That is all!"))
+    print(text_report_builder.parts.get_result())
